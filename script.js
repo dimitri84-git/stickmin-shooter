3aede0c8cf94b970943c7933b1b73a8403e6616a
Dimitri <dimitri.ray.boyce@gmail.com>
2024-10-01 17:48:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.136.0';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls.js';
 
